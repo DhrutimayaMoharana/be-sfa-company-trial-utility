87bc3203e1a5d4ab3ba50251b7dddee1999ce4f6
Dhrutimaya <dhrutimaya.moharana@rapidsoft.co.in>
2023-04-24 15:31:09 +0530

    Update BeSfaCompanyTrialUtilityApplication.java

--- src/main/java/com/watsoo/sfa/trial/BeSfaCompanyTrialUtilityApplication.java
+++ src/main/java/com/watsoo/sfa/trial/BeSfaCompanyTrialUtilityApplication.java
@@ -6,11 +6,12 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class BeSfaCompanyTrialUtilityApplication {
+public class BeSfaCompanyTrialUtilityApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BeSfaCompanyTrialUtilityApplication.class, args);
